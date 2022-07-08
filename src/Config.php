c12959b5a1a366b6045237927d37d43e0306cf91
kasim.yilmaz <kasim.yilmaz@pts.net>
2022-07-08 16:53:50 +0300

    config

---
+++ src/Config.php
@@ -0,0 +1,8 @@
+<?php
+namespace ksmylmz\ptsapi;
+class  Config
+{
+    const USERNAME="username";
+    const PASSWORD="password";
+    const WSDL_URL="https://pts.net:5161/api/shipmentv4/api.php?wsdl";
+}
