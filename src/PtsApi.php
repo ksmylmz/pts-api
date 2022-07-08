47c979b31846a4936df4250c0c910a2109d14b5e
kasim.yilmaz <kasim.yilmaz@pts.net>
2022-07-08 17:14:38 +0300

    getlabel model | action

--- src/PtsApi.php
+++ src/PtsApi.php
@@ -1,6 +1,7 @@
 <?php
 namespace ksmylmz\ptsapi;
 use ksmylmz\ptsapi\Config;
+use ksmylmz\ptsapi\model\GetLabel;
 use ksmylmz\ptsapi\model\Tracking;
 
 class PtsApi 
@@ -12,7 +13,15 @@ class PtsApi
     {
         $this->service = new Service();
     }
-
+    
+    /**
+     * Tracking
+     *
+     * @param  string $ptsRefferance
+     * @param  string $customerRefferance
+     * @param  string $language
+     * @return \ksmylmz\ptsapi\model\Response
+     */
     public function Tracking($ptsRefferance,$customerRefferance,$language="EN")
     {
         $payload  = new Tracking();
@@ -21,6 +30,24 @@ class PtsApi
         $payload->ptsno = (empty($ptsRefferance))?"":$ptsRefferance;
         $payload->siparisno = (empty($customerRefferance))?"":$customerRefferance;
         $payload->dil = $language;
+        return $this->service->sendRequest("tracking",$payload);
+    }    
+    /**
+     * GetLabel
+     *
+     * @param  string $ptsRefferance
+     * @param  string $customerRefferance
+     * @param  string $labelCode
+     * @return \ksmylmz\ptsapi\model\Response
+     */
+    public function GetLabel($ptsRefferance,$customerRefferance,$labelCode=1)
+    {
+        $payload  = new GetLabel();
+        $payload->kullanici = Config::USERNAME;
+        $payload->sifre = Config::PASSWORD;
+        $payload->ptsno = (empty($ptsRefferance))?"":$ptsRefferance;
+        $payload->siparisno = (empty($customerRefferance))?"":$customerRefferance;
+        $payload->etiket = $labelCode;
         return $this->service->sendRequest("tracking",$payload);
     }
 }
---
+++ src/model/GetLabel.php
@@ -0,0 +1,35 @@
+<?php
+namespace ksmylmz\ptsapi\model;
+class GetLabel 
+{    
+    /**
+     * kullanici
+     *
+     * @var string
+     */
+    public $kullanici;    
+    /**
+     * sifre
+     *
+     * @var string
+     */
+    public $sifre;    
+    /**
+     * ptsno
+     *
+     * @var string
+     */
+    public $ptsno;    
+    /**
+     * siparisno
+     *
+     * @var string
+     */
+    public $siparisno;    
+    /**
+     * dil
+     *
+     * @var string
+     */
+    public $etiket;
+}
